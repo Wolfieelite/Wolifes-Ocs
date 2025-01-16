51411d315006980213f60946ba49ea4b1f623309
wolfieelite <martetaylor333@gmail.com>
2025-01-16 09:59:44 -0800

    added the pages to the project and set up routes

---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      {/* title section */}
+      <header></header>
+
+      {/* section for organized dropdown menu */}
+      <div></div>
+
+      {/* main content for the cards */}
+      <main></main>
+
+    </div>
+  )
+}
