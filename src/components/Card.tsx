db2a566c02c88526658658528cac55282ee3043f
wolfieelite <martetaylor333@gmail.com>
2025-01-25 22:03:48 -0800

    added props to card ready to pass dynamic data when created more deata

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,44 +1,39 @@
-
+import { FaRightLong } from "react-icons/fa6";
 import { MdTextFields } from "react-icons/md";
 import { PiImageSquare } from "react-icons/pi";
 
+interface CardProps{
+  name: string,
+  age?: number,
+  bio: string,
+}
 
-
-function Card() { 
+function Card({ name, age, bio }: CardProps) {
   return (
-    <article className="flex flex-col border-2 border-black rounded-t-xl bg-slate-900/20">
-      <header className="border-black py-5">
-        <h1 className="text-lg flex justify-center">Character name | age?</h1>
+    <article className="flex flex-col border-2 border-black rounded-t-3xl bg-orange-500/40 dark:bg-slate-950/20">
+      <header className="border-black py-5 bg-inherit rounded-3xl">
+        <h1 className="text-lg flex justify-center">{name} | {age}</h1>
       </header>
 
-      <nav className="flex justify-around bg-inherit">
-        <button className="px-7 py-4"><MdTextFields size={25}/></button>
-        <button className="px-7 py-4"><PiImageSquare size={25}/></button>
+      <nav className="grid grid-cols-2">
+        <button className="place-self-center hover:bg-black">
+          <MdTextFields size={25} />
+        </button>
+        <button className="px-7 py-4 place-self-center">
+          <PiImageSquare size={25} />
+        </button>
       </nav>
 
       {/* content: */}
-      <div className="px-8 min-h-96 max-h-96 overflow-scroll bg-inherit">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error itaque
-        blanditiis fugiat ex temporibus reiciendis quos ullam magnam sint quo
-        facere nostrum at reprehenderit laudantium distinctio, illo inventore
-        expedita hic? Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        Ipsam molestias a voluptatem quod non! Non voluptates eum tempora
-        reiciendis dignissimos, voluptatum commodi quibusdam aliquam quas
-        laudantium maxime ut, consequuntur fugit! Molestias earum dicta impedit?
-        Eos quisquam magnam magni quibusdam dolores corporis natus odio alias,
-        labore harum quod eligendi repellendus inventore similique perspiciatis
-        fugiat officiis voluptates aut omnis impedit necessitatibus id. Minus ex
-        eum obcaecati labore quasi aliquid, voluptatibus rerum atque? Quibusdam
-        natus dolores deserunt distinctio, quas ipsam officia, voluptatem dicta
-        illo minima, quia unde architecto optio voluptatibus? Soluta, id
-        aliquid! Aliquid fugit rerum beatae et cumque minima nulla
-        exercitationem eligendi unde reprehenderit facere iste quasi qui saepe
-        ducimus molestiae cupiditate autem est, consectetur, architecto
-        temporibus necessitatibus dolor? Ad, modi blanditiis.
-      </div>
+      <div className="px-8 min-h-96 max-h-96 overflow-scroll">{ bio }</div>
 
-      <div className="w-full py-3">
-        <button className="w-full text-center">Show more</button>
+      <div className="grid grid-cols-1 w-full py-3 bg-inherit">
+        <button className="flex gap-10 place-content-end">
+          Bio of character
+          <span>
+            <FaRightLong size={30} />
+          </span>
+        </button>
       </div>
     </article>
   );
